7c95a9d11ef5bf64176e8d1a19bf343d6a2e0573
kanchana prathibha <132212636+kanchana-prathibha@users.noreply.github.com>
2023-11-14 23:07:12 +0530

    add snow otherpages

--- js/xmas.js
+++ js/xmas.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
     function createSnowflake() {
         const snowflake = document.createElement('div');
         snowflake.classList.add('snowflake');
-        snowflake.innerHTML = '.';
+        snowflake.innerHTML = '<i class="fa-solid fa-snowflake fa-2xs"></i>';
 
-        const randomSize = Math.random() * 1.5 + 0.5;
+        const randomSize = Math.random() * 1 + 0.5;
         snowflake.style.fontSize = `${randomSize}em`;
 
         const randomPosition = Math.random() * 100;
         snowflake.style.left = `${randomPosition}vw`;
 
-        const animationDuration = Math.random() * 2 + 2;
+        const animationDuration = Math.random() * 10 + 15;
         snowflake.style.animationDuration = `${animationDuration}s`;
 
         snowflakesContainer.appendChild(snowflake);
